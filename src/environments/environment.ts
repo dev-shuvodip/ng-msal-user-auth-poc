3d84611e0dacf385e36ce83f5c924b02c5fa6621
Shuvodip Ray <shuvodip@spdev1200.onmicrosoft.com>
2022-12-24 13:21:31 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Application_Id: 'e71bbeef-0deb-4187-89de-12bb2d931188',
+  Tenant_Info: '4f11d626-ffd9-414f-85c3-ef285eeeca4a',
+  Cloud_Instance_Id: 'https://login.microsoftonline.com/',
+  Redirect_Uri: 'http://localhost:4200/'
 };
 
-/*
+/*f
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
  *
